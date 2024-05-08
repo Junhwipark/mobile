8069a47ac45d351289ae25a8fe0d24bec9ff17be
NextDoorbutter <131065002+Nextdoorbutter@users.noreply.github.com>
2024-05-08 17:45:42 +0900

    서브 액티비티 2개 추가
    검색 액티비티 / 마이페이지 액티비티

--- app/src/main/java/com/example/mobile/MainActivity.java
+++ app/src/main/java/com/example/mobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button main_button = (Button) findViewById(R.id.main_button);
+
+        Button search_button = (Button) findViewById(R.id.search_button);
+        search_button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+        });
+
+        Button mypage_button = (Button) findViewById(R.id.mypage_button);
+        mypage_button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MypageActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
